6c0468c4e3b89972c341c9004a979f1c70f0f451
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-05-11 16:32:37 +0530

    update cursor

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -3,43 +3,27 @@ import { gsap } from "gsap";
 
 function Cursor() {
   useEffect(() => {
-    const cursor = document.getElementById("cursor");
-
-    // 🧠 Move the cursor
-    const handleMouseMove = (e) => {
-      gsap.to(cursor, {
-        x: e.clientX,
-        y: e.clientY,
-        duration: 0.3,
+    const handleMouseMove = () => {
+      const { clientX, clientY } = event;
+      gsap.to("#cursor", {
+        x: clientX,
+        y: clientY,
+        duration: 0.7,
         ease: "power4.out",
       });
     };
 
-    // 🧠 Scale up/down on a/button hover using event delegation
-    const handleMouseOver = (e) => {
-      if (["A", "BUTTON"].includes(e.target.tagName)) {
-        gsap.to(cursor, { scale: 1.5, duration: 0.2 });
-      }
-    };
-
-    const handleMouseOut = (e) => {
-      if (["A", "BUTTON"].includes(e.target.tagName)) {
-        gsap.to(cursor, { scale: 1, duration: 0.2 });
-      }
-    };
+    window.addEventListener("mousemove", handleMouseMove);
 
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseover", handleMouseOver);
-      document.removeEventListener("mouseout", handleMouseOut);
     };
   }, []);
-
   return (
     <div
       id="cursor"
-      className="fixed -top-6 -left-6 size-12 rounded-full border-1 border-white/70 z-10 pointer-events-none"
-    />
+      className="fixed -top-6 -left-6 size-12 rounded-full border border-white pointer-events-none"
+    ></div>
   );
 }
 
